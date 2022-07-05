8a38f1a614d3bf0156f96313cc7a7dafb5089ef0
roxascarl <108616473+griszlybear@users.noreply.github.com>
2022-07-05 13:22:07 +0800

    v2

--- hello.py
+++ hello.py
@@ -4,6 +4,6 @@ mensahe = "Hello World"
 name = "Carl"
 name1 = "Buchoy"
 
-print(f"My name is {name} and {msg}")
-print(f"My name is {name1} and {msg}")
+print(f"My name is {name} and {mensahe}")
+print(f"My name is {name1} and {mensahe}")
 
